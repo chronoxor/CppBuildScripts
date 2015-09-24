65313c4b14161e0268f5b1f0b637b71586fc86aa
Ivan Shynkarenka <chronoxor@gmail.com>
2015-09-25 01:38:02 +0300

    Update doxygen script

--- 05-doxygen-vs2015.bat
+++ 05-doxygen-vs2015.bat
@@ -1,3 +1,3 @@
 cd ..
 cd build
-for %%f in (*-doxygen.vcxproj) do "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" %%~f /p:Configuration=Release
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" doxygen.vcxproj /p:Configuration=Release
