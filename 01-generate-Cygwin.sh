a98cff114193b648689024cf9ed98ab1e0b72423
Ivan Shynkarenka <chronoxor@gmail.com>
2016-09-22 02:25:41 +0300

    Rollback

--- 01-generate-Cygwin.sh
+++ 01-generate-Cygwin.sh
@@ -2,5 +2,5 @@
 cd ..
 mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Unix Makefiles" ..
+cmake -DCMAKE_LEGACY_CYGWIN_WIN32=0 -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "Unix Makefiles" ..
 cd ../scripts
