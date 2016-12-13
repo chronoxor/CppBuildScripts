574a5a0f6ece1201b1137b4877d0fd72ab4d7630
Ivan Shynkarenka <chronoxor@gmail.com>
2016-12-13 03:26:18 +0300

    update

--- MinGW/01-generate.bat
+++ MinGW/01-generate.bat
@@ -1,6 +1,6 @@
 cd ../..
 if not exist "temp" mkdir temp
 cd temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -G "MinGW Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "MinGW Makefiles" ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../build/MinGW
--- VisualStudio/01-generate.bat
+++ VisualStudio/01-generate.bat
@@ -1,6 +1,6 @@
 cd ../..
 if not exist "temp" mkdir temp
 cd temp
-cmake -G "Visual Studio 14 2015 Win64" ..
+cmake -DCMAKE_INSTALL_PREFIX=../temp/install -G "Visual Studio 14 2015 Win64" ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../build/VisualStudio
