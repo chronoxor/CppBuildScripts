a6ee4fee8d51756cb635aebb8f5d102e01b049dd
Ivan Shynkarenka <chronoxor@gmail.com>
2017-04-10 21:58:12 +0300

    Migrate to Visual Studio 2017

--- VisualStudio/05-doxygen.bat
+++ VisualStudio/05-doxygen.bat
@@ -2,7 +2,7 @@ cd ../../documents
 for /F "tokens=* USEBACKQ" %%F in (`git config --get remote.origin.url`) do (set remote=%%F)
 git clone --depth=5 -b gh-pages %remote% html
 cd ../temp
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" doxygen.vcxproj /p:Configuration=RelWithDebInfo
+MSBuild doxygen.vcxproj /p:Configuration=RelWithDebInfo
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../documents/html
 git add -A .
