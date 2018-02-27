7c6e61e2dd67fea46c9aae56d695ea167ab1313a
Ivan Shynkarenka <chronoxor@gmail.com>
2018-02-28 01:17:21 +0300

    update

--- mingw.bat
+++ mingw.bat
@@ -3,6 +3,8 @@ call 01-generate.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
 call 02-build.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
+call 03-tests.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 call 04-install.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
 call 05-doxygen.bat
