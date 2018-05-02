f9954a83345372287f1d21139ec6fe74bfadca3d
Ivan Shynkarenka <chronoxor@gmail.com>
2018-05-02 11:47:10 +0300

    Remove CMAKE_LEGACY_CYGWIN_WIN32

--- Cygwin/01-generate.sh
+++ Cygwin/01-generate.sh
@@ -3,5 +3,5 @@ set -e
 cd ../..
 mkdir -p temp
 cd temp
-cmake -DCMAKE_LEGACY_CYGWIN_WIN32=0 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
 cd ../build/Cygwin
