18b5c282a5315a2d437ea1aeca04fd90508a8be7
Ivan Shynkarenka <chronoxor@gmail.com>
2018-12-21 19:42:02 +0300

    update

--- Cygwin/01-generate.sh
+++ Cygwin/01-generate.sh
@@ -4,5 +4,5 @@ tar -C / -xvf cmake-3.13.1-1.tar.xz
 cd ../..
 mkdir -p temp
 cd temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
+/usr/bin/cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
 cd ../build/Cygwin
