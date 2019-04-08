d8f7cfdad083e72b5093bbe6c5fcc7d165c3a15b
Ivan Shynkarenka <chronoxor@gmail.com>
2019-04-08 12:38:35 +0300

    Visual Studio 2019

--- VisualStudio/01-generate.bat
+++ VisualStudio/01-generate.bat
@@ -2,6 +2,6 @@
 cd ../..
 if not exist "temp" mkdir temp
 cd temp
-cmake -DCMAKE_INSTALL_PREFIX=../temp/install -G "Visual Studio 15 2017 Win64" ..
+cmake -DCMAKE_INSTALL_PREFIX=../temp/install -G "Visual Studio 16 2019" -A x64 ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../build/VisualStudio
