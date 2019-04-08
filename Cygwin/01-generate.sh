f5cf2956acebadf1e2635b46066f3b7c22fe9b3c
Ivan Shynkarenka <chronoxor@gmail.com>
2019-04-08 11:29:36 +0300

    update

--- Cygwin/01-generate.sh
+++ Cygwin/01-generate.sh
@@ -4,5 +4,5 @@ tar -C / -xvf cmake-3.13.1-1.tar.xz
 cd ../..
 mkdir -p temp
 cd temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
+cmake -DOPENSSL_ROOT_DIR=/usr/local/ssl -DOPENSSL_LIBRARIES=/usr/local/ssl/lib -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
 cd ../build/Cygwin
