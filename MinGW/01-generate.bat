54b58db72bb271343f4ea401366a5346c4f74113
Ivan Shynkarenka <chronoxor@gmail.com>
2020-09-17 13:42:03 +0300

    update

--- MinGW/01-generate.bat
+++ MinGW/01-generate.bat
@@ -2,6 +2,6 @@
 cd ../..
 if not exist "temp" mkdir temp
 cd temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "MinGW Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../temp/install -G "MinGW Makefiles" ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 cd ../build/MinGW
--- Unix/01-generate.sh
+++ Unix/01-generate.sh
@@ -3,5 +3,5 @@ set -e
 cd ../..
 mkdir -p temp
 cd temp
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../temp/install -G "Unix Makefiles" ..
 cd ../build/Unix
