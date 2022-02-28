bfce7c766f0ef701c6d2fe540506395c957f191f
Ivan Shynkarenka <chronoxor@gmail.com>
2022-03-01 01:39:55 +0300

    Remove MSYS build

--- MSYS/01-generate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-cd ../..
-mkdir -p temp
-cd temp
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../temp/install -G "MSYS Makefiles" ..
-cd ../build/MSYS
--- MSYS/02-build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-cd ../..
-cd temp
-make -j8 all
-cd ../build/MSYS
