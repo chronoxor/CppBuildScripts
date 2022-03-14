8a3fb2c7799c14464f87d5dcd83e03c02e675309
Ivan Shynkarenka <Ivan_Shynkarenka@epam.com>
2022-03-14 23:04:25 +0300

    Update 05-doxygen.sh

--- Unix/05-doxygen.sh
+++ Unix/05-doxygen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ../../documents
 remote=`git config --get remote.origin.url`
+remote="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@${remote#https://}"
 git clone --depth=5 -b gh-pages ${remote} html
 cd ../temp
 make -j8 doxygen
 cd ../documents/html
 git add -A .
 git commit -am "Update generated documentation"
-remote="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@${remote#https://}"
 git push ${remote}
 cd ../../build/Unix
