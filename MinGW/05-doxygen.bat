7a9e1894b68c1e44d55cd5a58ee315f668407f2d
Ivan Shynkarenka <Ivan_Shynkarenka@epam.com>
2022-07-10 03:19:28 +0400

    Update Doxygen build script

--- MinGW/05-doxygen.bat
+++ MinGW/05-doxygen.bat
@@ -1,11 +1,12 @@
 @echo off
+cd ../..
 
 rem Setup git remote parameter
 for /F "tokens=* USEBACKQ" %%F in (`git config --get remote.origin.url`) do (set remote=%%F)
 set remote="https://%GITHUB_ACTOR%:%GITHUB_TOKEN%@%remote:https://=%"
 
 rem Generate documentation
-cd ../../documents
+cd documents
 git clone --depth=5 -b gh-pages %remote% html
 cd ../temp
 mingw32-make -j4 doxygen
--- Unix/05-doxygen.sh
+++ Unix/05-doxygen.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+cd ../..
 
 # Setup git remote parameter
 remote=`git config --get remote.origin.url`
 remote="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@${remote#https://}"
 
 # Generate documentation
-cd ../../documents
+cd documents
 git clone --depth=5 -b gh-pages ${remote} html
 cd ../temp
 make -j4 doxygen
